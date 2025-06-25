30f4e709d794889be67de4575fe48e9e3eeb4f87
eduardaro <eduarda.rocha@estudante.ifms.edu.br>
2025-06-25 11:01:52 -0400

    dudassino

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -10,6 +10,7 @@
         <li class="nav-item"><a class="nav-link active" href="/unidades">Unidades</a></li>
         <li class="nav-item"><a class="nav-link active" href="/calculadora">Calculadora</a></li>
         <li class="nav-item"><a class="nav-link active" href="/dicionario">Dicionário</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/cassino">Cassino</a></li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown">Dropdown</a>
           <ul class="dropdown-menu">
---
+++ src/routes/cassino/megasorte/+page.svelte
@@ -0,0 +1,1 @@
+<h1> MegaSorte</h1>
